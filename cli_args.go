e80d3d4014b05a44ef991d2f597f0b946d467421
jamieastley <6836556+jamieastley@users.noreply.github.com>
2022-10-28 18:02:47 +1100

    add cli flags

--- cli_args.go
+++ cli_args.go
@@ -4,13 +4,19 @@ import "flag"
 
 func parseArgs() *CliArgs {
 	path := flag.String("path", "", "Path of the test directory")
+	outputFile := flag.String("out", "", "Filename of the output test wrapper")
+	verbose := flag.Bool("v", false, "Enables verbose logging")
 	flag.Parse()
 
 	return &CliArgs{
-		Path: *path,
+		Path:       *path,
+		OutputFile: *outputFile,
+		Verbose:    *verbose,
 	}
 }
 
 type CliArgs struct {
-	Path string
+	Path       string
+	OutputFile string
+	Verbose    bool
 }
